f8ff5775f6a1f11ab1bed9e3dbbe288686854863
Sudip Das <pprishh@gmail.com>
2023-10-23 12:01:37 +0530

    format

--- assignment 6/4.c
+++ assignment 6/4.c
@@ -1,5 +1,5 @@
-//palindrome function
-#include<stdio.h>
+// palindrome function
+#include <stdio.h>
 void for_check(int);
 int sum_digits(int);
 int multi_digits(int);
@@ -7,7 +7,7 @@ int main()
 {
     int n, sum;
     printf("Enter a number: ");
-    scanf("%d",&n);
+    scanf("%d", &n);
     for_check(n);
 
     return 0;
@@ -15,47 +15,46 @@ int main()
 
 void for_check(int a)
 {
-    int palin=0,remin,temp=a;
+    int palin = 0, remin, temp = a;
     int sum, mult;
-    while(temp>0)
+    while (temp > 0)
     {
-        remin=temp%10;
-        palin = 10*palin + remin;
-        temp=temp/10;
+        remin = temp % 10;
+        palin = 10 * palin + remin;
+        temp = temp / 10;
     }
-    if(palin==a)
+    if (palin == a)
     {
-       sum=sum_digits(a); 
-       printf("%d", sum);
+        sum = sum_digits(a);
+        printf("%d", sum);
     }
     else
     {
-    mult = multi_digits(a);
-    printf("%d", mult);
+        mult = multi_digits(a);
+        printf("%d", mult);
     }
-
 }
 int sum_digits(int a)
 {
-    int total=0;
-    while(a>0)
+    int total = 0;
+    while (a > 0)
     {
         int remin;
-        remin=a%10;
-        total = total+remin;
-        a=a/10;
+        remin = a % 10;
+        total = total + remin;
+        a = a / 10;
     }
     return total;
 }
 int multi_digits(int a)
 {
-    int multi=1;
-    while(a>0)
+    int multi = 1;
+    while (a > 0)
     {
-        int remin; 
-        remin=a%10;
-        multi = multi*remin;
-        a=a/10;
+        int remin;
+        remin = a % 10;
+        multi = multi * remin;
+        a = a / 10;
     }
     return multi;
 }
--- assignment 6/5.c
+++ assignment 6/5.c
@@ -1,19 +1,20 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    int n,remin, pelin=0;
+    int n, remin, pelin = 0;
     printf("Enter a num: ");
-    scanf("%d",&n);
-    int tempn=n;
-    while(tempn>0)
+    scanf("%d", &n);
+    int tempn = n;
+    while (tempn > 0)
     {
-        remin=tempn%10;
-        pelin = 10*pelin + remin;
-        tempn=tempn/10;
+        remin = tempn % 10;
+        pelin = 10 * pelin + remin;
+        tempn = tempn / 10;
     }
-    if(pelin==n)
+    if (pelin == n)
     {
-       printf("palindrome");
+        printf("palindrome");
     }
+    printf("add not");
     return 0;
 }
